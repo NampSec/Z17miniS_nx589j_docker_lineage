771b3c52bd9e47e6f560c55432d992ee8410b966
NampSec <playload@qq.com>
2023-03-05 12:32:59 +0800

    overlyfs patched

---
+++ build.sh
@@ -0,0 +1,2 @@
+make O=out overlayfs_nx589j_defconfig
+make O=out -j4
---
+++ envsetup.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/home/yang/program/toolchain/arm64/google_gcc/aarch64-linux-android-4.9/bin
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-android-
